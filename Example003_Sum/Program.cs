4286021c549d2f3fff40476af108ba0fc35effc8
Artyom <arteman007@gmail.com>
2023-09-23 14:32:19 +0300

    add random type

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 123;
-int numberB = 5647;
+﻿int numberA = new Random().Next(1, 1000); // 1 2 3 4 5 6 7 8 9 !!!
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 1000);
+Console.WriteLine(numberB);
 int result = (numberA + numberB);
 Console.WriteLine (result);
